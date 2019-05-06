c6cab29039411b7636f3f2495a16b326707ec5ef
Golaj <ch.rehn90@gmail.com>
2019-05-06 15:23:12 +0200

    Added transport details for routes within a search

--- src/components/Card.js
+++ src/components/Card.js
@@ -3,18 +3,45 @@ import React from 'react';
 
 class Card extends React.Component {
 
+
     render(props) {
         const place = this.props;
+        const price = place.routes[0].indicativePrices[0].price;
+
         console.log("place in Card")
         console.log(place);
+
+        function getHours(min) {
+            return Math.trunc(min / 60) + "h";
+        }
+
+        function getMin(min) {
+            return min % 60 + "min";
+        }
+
+        function getKm(distance){
+            return Math.trunc(distance) + "km";
+        }
+
         return (
             <div>
                 <div className="info">
                     <div>From: {place.places[0].longName}</div>
                     <div>To: {place.places[1].longName}</div>
-                    
-                    <div>Transport type: {place.routes[0].name}</div>
-                    <div>distance: {place.routes[0].distance}</div>
+
+                    <div classeName="allRoutes">All transports: {place.routes.map(element => (
+                        <div className="routeSegment"><br></br>
+                            Transport type: {element.name} <br></br>
+                            Distance: {getKm(element.distance)} <br></br>
+                            Total Duration: {getHours(element.totalDuration)} {getMin(element.totalDuration)} <br></br>
+                            Price: {element.indicativePrices ? element.indicativePrices.map(x => (
+
+                                <span>{x.priceLow ? x.priceLow + " - " + x.priceHigh
+                                : x.name ? x.name + " " + x.price : x.price} {x.currency} </span> 
+                            )) : "Not available"}
+                        </div>
+                    ))}</div>
+
                 </div>
             </div>
         )
