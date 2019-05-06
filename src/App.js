41fd25858f2b4a2013b508617981579e071568e6
nurmatron <oscarnurmi1@gmail.com>
2019-05-06 15:10:05 +0200

    logga och css

--- src/App.js
+++ src/App.js
@@ -1,13 +1,19 @@
 import React, { Component } from 'react';
 import './App.css';
-
 import Form from "./components/Form"
 import Route from "./components/Route"
-
+import logga from "./pics/projektlogga.png"
 class App extends Component {
   state = {
     places: []
   };
+       /*
+        <div className ="ImageDiv">
+          <img src={logga} alt ="logga" className="projektlogga"/>
+          </div>  
+       */
+
+
 
   addNewRoute = (routeData) => {
     this.setState(prevState => ({
@@ -18,11 +24,33 @@ class App extends Component {
   render() {
     return (
       <div className="App">
-        <Form onSubmit={this.addNewRoute}/>
-        <Route places={this.state.places} />
-      </div>
-    );
-  }
-}
+    
+          <header className="ourHeader">
+          <div className ="ImageDiv">
+          <img src={logga} alt ="logga" className="projektlogga"/>
+          </div >  
+            <div className="headerList">
+            <li className ="listItems"><a href="/"className="colorChanger">sök resor </a></li>
+            <li className ="listItems"><a href="/about"className="colorChanger">läs om eventet </a></li>
+            <li className ="listItems"><a href="/contact" className="colorChanger">läs om våra orter </a></li>
+            <li className ="listItems"><a href="/contact"className="colorChanger">se rekommendat-ioner</a></li>
+      
+            </div>
+          </header>
+            
 
-export default App;
+          <Form onSubmit={this.addNewRoute} className="onSubmit" />
+          <Route places={this.state.places}  className ="routePlaces"/>
+
+          <div className="testModul"> detta är en test modul</div>
+          <div className="testModul"> detta är en test modul</div>
+          <div className="testModul"> detta är en test modul</div>
+          <div className="testModul"> detta är en test modul</div>
+
+          
+      </div>
+        );
+      }
+    }
+    
+    export default App;
