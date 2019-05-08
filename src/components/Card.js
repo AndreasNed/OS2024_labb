013888f6fd8893af981a16946ec2d5e130167041
Christoffer Rehn <ch.rehn90@gmail.com>
2019-05-08 10:37:57 +0200

    Some refactoring, disabled Route component and inserted text for empty search result

--- src/components/Card.js
+++ src/components/Card.js
@@ -3,12 +3,11 @@ import React from 'react';
 
 class Card extends React.Component {
 
-
     render(props) {
-        const place = this.props;
+        const data = this.props.routeData;
 
         console.log("place in Card")
-        console.log(place);
+        console.log(data);
 
         function getHours(min) {
             return Math.trunc(min / 60) + "h";
@@ -21,25 +20,29 @@ class Card extends React.Component {
         function getKm(distance){
             return Math.trunc(distance) + "km";
         }
+        
+        const routeSegment = data.routes.length ?
+             (<div className="allRoutes">All transports: {data.routes.map(element => (
+                <div className="routeSegment"><br/>
+                    <div>Transport type: {element.name}</div>
+                    <div>Distance: {getKm(element.distance)}</div>
+                    <div>Total Duration: {getHours(element.totalDuration)} {getMin(element.totalDuration)}</div>
+                    <div>Price: {element.indicativePrices ? element.indicativePrices.map(x => (
+                        <span>{x.priceLow ? x.priceLow + " - " + x.priceHigh
+                        : x.name ? x.name + " " + x.price : x.price} {x.currency} </span> 
+                    )) : "Not available"}</div>
+                </div>
+            ))}</div>)
+            : <div>Sorry, there's no transports for that journy</div>
+        
 
         return (
             <div>
                 <div className="info">
-                    <div>From: {place.places[0].longName}</div>
-                    <div>To: {place.places[1].longName}</div>
+                    <div>From: {data.places[0].longName}</div>
+                    <div>To: {data.places[1].longName}</div>
 
-                    <div className="allRoutes">All transports: {place.routes.map(element => (
-                        <div className="routeSegment"><br></br>
-                            Transport type: {element.name} <br></br>
-                            Distance: {getKm(element.distance)} <br></br>
-                            Total Duration: {getHours(element.totalDuration)} {getMin(element.totalDuration)} <br></br>
-                            Price: {element.indicativePrices ? element.indicativePrices.map(x => (
-
-                                <span>{x.priceLow ? x.priceLow + " - " + x.priceHigh
-                                : x.name ? x.name + " " + x.price : x.price} {x.currency} </span> 
-                            )) : "Not available"}
-                        </div>
-                    ))}</div>
+                    {routeSegment}
 
                 </div>
             </div>
